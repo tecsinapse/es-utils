33a87f82d10ccfaf039e0f455d597e915ed8de24
Wellington Oliveira <well.oliveira.snt@gmail.com>
2019-01-14 21:31:37 -0300

    Criando object utils

--- src/index.js
+++ src/index.js
@@ -1,75 +1,106 @@
-'use strict';
+"use strict";
 
 var lodash = require('lodash');
 
-exports.printMsg = function () {
-  console.log("This is a message from the demo package");
+var objUtils = {
+  omitDeep: function (obj, key) {
+    const keys = Object.keys(obj);
+    const newObj = {};
+
+    for (const i of keys) {
+      if (i !== key) {
+        const val = obj[i];
+        if (Array.isArray(val)) {
+          newObj[i] = objUtils.omitDeepArrayWalk(val, key);
+        } else if (typeof val === 'object' && val !== null) {
+          newObj[i] = objUtils.omitDeep(val, key);
+        } else {
+          newObj[i] = val;
+        }
+      }
+    }
+    return newObj;
+  },
+
+  omitDeepArrayWalk : function (arr, key) {
+    return arr.map(function (val) {
+      return Array.isArray(val) ? objUtils.omitDeepArrayWalk(val, key) : typeof val === 'object' ? objUtils.omitDeep(val, key) : val;
+    });
+  },
+
+  flatten : function (arrayOfArrays) {
+    return arrayOfArrays.reduce(function (a, b) {
+      return a.concat(b);
+    }, [])
+  },
+
+  getFirstFromSingleElementArray: function (array = []) {
+    return array.length === 1 ? array[0] : null
+  },
+
+  getAnyFromArray: function(array = []) {
+    return array.length > 0 ? array[0] : null;
+  },
+
+  getLastFromArrayOrObject: function(array = []) {
+    return array.length > 0 ? array[array.length - 1] : {};
+  },
+
+  getFirstFromSingleElementArrayNotNull: function(array = []) {
+    return array.length === 1 ? array[0] : {};
+  },
+
+  isEmptyOrNull: function(value) {
+    return value == null || value.length === 0;
+  },
+
+  isNotEmptyOrNull: function(value) {
+    return !(objUtils.isEmptyOrNull(value));
+  },
+
+  verifyIfString: function(value) {
+    return value && typeof value === 'string';
+  },
+
+  isStringNotBlank: function(value) {
+    return objUtils.isNotEmptyOrNull(value) && value.trim().length;
+  },
+
+  truncateString: function(string, max = 30) {
+    return string.substr(0, max - 1) + (string.length > max ? '...' : '');
+  },
+
+  removeDuplicates: function(myArr, prop) {
+    const arrayOfKeys = myArr.map(function (mapObj) {
+      return mapObj[prop];
+    });
+    return myArr.filter(function (obj, pos) {
+      return arrayOfKeys.indexOf(obj[prop]) === pos;
+    });
+  },
+
+  resolveObj: function(path, obj = null) {
+    const newPath = path.split('.');
+    let current = obj;
+    while (newPath.length) {
+      if (typeof current !== 'object' || current === null) return null;
+      current = current[newPath.shift()];
+    }
+    return current;
+  },
+
+  concat: function(x, y) {
+    return x.concat(y);
+  },
+
+  flatMap: function(f, xs) {
+    return xs.map(f).reduce(concat, []);
+  },
+
+  cleanArray : function (a) {
+    return lodash.uniq(a.filter(n => n));
+  }
+
 };
 
-exports.flatten = function (arrayOfArrays) {
-  return arrayOfArrays.reduce((a, b) => a.concat(b), [])
-};
-
-exports.getFirstFromSingleElementArray = function(array = []) {
-  return array.length === 1 ? array[0] : null
-};
-
-// exports.getAnyFromArray = (array = []) =>
-//   array.length > 0 ? array[0] : null;
-//
-// exports.getLastFromArrayOrObject = (array = []) =>
-//   array.length > 0 ? array[array.length - 1] : {};
-//
-// exports.getFirstFromSingleElementArrayNotNull = (array = []) =>
-//   array.length === 1 ? array[0] : {};
-//
-// exports.isEmptyOrNull = value => value == null || value.length === 0;
-// exports.isNotEmptyOrNull = value => !isEmptyOrNull(value);
-// exports.verifyIfString = value => value && typeof value === 'string';
-// exports.isStringNotBlank = value =>
-//   isNotEmptyOrNull(value) && value.trim().length;
-//
-// exports.truncateString = (string, max = 30) =>
-//   string.substr(0, max - 1) + (string.length > max ? '...' : '');
-//
-// exports.omitDeepArrayWalk = (arr, key) =>
-//   arr.map(val => {
-//     if (Array.isArray(val)) return omitDeepArrayWalk(val, key);
-//     // eslint-disable-next-line
-//     else if (typeof val === 'object') return omitDeep(val, key);
-//     return val;
-//   });
-//
-// exports.omitDeep = (obj, key) => {
-//   const keys = Object.keys(obj);
-//   const newObj = {};
-//   keys.forEach(i => {
-//     if (i !== key) {
-//       const val = obj[i];
-//       if (Array.isArray(val)) newObj[i] = omitDeepArrayWalk(val, key);
-//       else if (typeof val === 'object' && val !== null)
-//         newObj[i] = omitDeep(val, key);
-//       else newObj[i] = val;
-//     }
-//   });
-//   return newObj;
-// };
-//
-// exports.removeDuplicates = (myArr, prop) => {
-//   const arrayOfKeys = myArr.map(mapObj => mapObj[prop]);
-//   return myArr.filter((obj, pos) => arrayOfKeys.indexOf(obj[prop]) === pos);
-// };
-//
-// exports.resolveObj = (path, obj = null) => {
-//   const newPath = path.split('.');
-//   let current = obj;
-//   while (newPath.length) {
-//     if (typeof current !== 'object' || current === null) return null;
-//     current = current[newPath.shift()];
-//   }
-//   return current;
-// };
-//
-// exports.concat = (x, y) => x.concat(y);
-//
-// exports.flatMap = (f, xs) => xs.map(f).reduce(concat, []);
+module.exports = objUtils;
