5e72d32c33674c5ce8c02714e8977a29fc6600bc
Wellington Oliveira <well.oliveira.snt@gmail.com>
2019-09-01 22:28:25 -0400

    Rollup build - Removing logs - Ajusting travis deploy

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import autoExternal from 'rollup-plugin-auto-external';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import commonjs from 'rollup-plugin-commonjs';
 import localResolve from 'rollup-plugin-local-resolve';
 
@@ -13,44 +13,46 @@ export default [
   {
     input: 'src/index.js',
     output: [
-      { file: pkg.main, name: pkg.name, format: 'cjs' },
       {
-        file: pkg.browser,
+        file: pkg.main,
         name: pkg.name,
-        format: 'umd',
         globals: {
-          react: 'React',
-          'react-router': 'Link',
-          'react-transition-group': 'ReactTransitionGroup',
-          'styled-component': 'styled',
-          'bootstrap-styled': 'Jumbotron',
-          '@material-ui/core': 'material-ui',
-          '@material-ui/icons': 'material-ui',
-          classnames: 'cn',
+          lodash: 'lodash',
         },
+        format: 'cjs',
+        sourcemap: true,
       },
     ],
 
     plugins: [
+      peerDepsExternal(),
       builtins(),
       babel({
         exclude: ['node_modules/**'],
         extensions: ['.js'],
       }),
       localResolve(),
-      autoExternal(),
       resolve({
         jsnext: true,
         main: true,
         browser: true,
       }),
       commonjs({
+        include: 'node_modules/**',
         namedExports: {
-          'node_modules/@material-ui/core/colors/index.js': ['grey'],
+          'node_modules/lodash/lodash.js': ['uniqBy'],
         },
       }),
 
       filesize(),
     ],
+    onwarn(warning, warn) {
+      // skip certain warnings
+      if (warning.code === 'THIS_IS_UNDEFINED') {
+        return;
+      }
+      // Use default for everything else
+      warn(warning);
+    },
   },
 ];
--- src/log/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-exports.Logger = {
-  error(error) {
-    console.log(error);
-    // eslint-disable-next-line
-    Raven.captureException(new Error(error));
-  },
-};
